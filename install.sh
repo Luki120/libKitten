acc1fa16cd53bc37ed1ad9b1dd4d00ed21e4628b
luki120 <luki120@exerra.xyz>
2023-09-08 23:05:23 -0300

    install script

---
+++ install.sh
@@ -0,0 +1,34 @@
+# credits ⇝ https://github.com/MrGcGamer/LibGcUniversalDocumentation/blob/master/install.sh
+
+#!/usr/bin/env bash
+
+# Make sure the THEOS environment variable is set
+if [ -z "${THEOS}" ]; then
+	echo "THEOS environment variable is missing."
+	exit 1
+fi
+
+# Exit on error
+set -e
+
+# Be verbose
+set -v
+
+# Copy the dynamic libraries
+mkdir -p "${THEOS}/lib/iphone/rootless"
+cp Library/libkitten.dylib "${THEOS}/lib/"
+cp Library/libkitten_rootless.dylib "${THEOS}/lib/iphone/rootless/libkitten.dylib"
+
+# Copy the header files
+mkdir -p "${THEOS}/include/Kitten"
+cp Library/*.h "${THEOS}/include/Kitten/"
+
+# Copy the module map
+cp Library/_module.modulemap "${THEOS}/include/Kitten/module.modulemap"
+
+# We're done
+set +v
+
+echo
+echo "Installation successful!"
+echo
